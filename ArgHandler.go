ace649ff16dbb9e7c3cbb2cf41f155fd42bc8496
Noah Friedman <noahfriedman2@gmail.com>
2020-03-23 15:33:11 -0400

    Added code

--- ArgHandler.go
+++ ArgHandler.go
@@ -1,1 +1,22 @@
 package ArgHandler
+
+import "errors"
+
+type option struct {
+	short string
+	long  string
+	valid []string
+}
+
+func NewOption(short string, long string, valid []string) (*option, error) {
+	if len(short) != 1 {
+		return nil, errors.New("short argument required to create option, must be one character long")
+	}
+	if len(valid) == 0 {
+		return nil, errors.New("at least one valid argument to the option is required")
+	}
+	return &option{short: short, long: long, valid: valid}, nil
+}
+
+type argHandler struct {
+}
